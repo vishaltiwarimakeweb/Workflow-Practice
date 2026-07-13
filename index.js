0c4aab5690e771e2903295664e17f2f081eabbf4
Vishal Tiwari <vishaltiwari@makewebbetter.com>
2026-07-13 12:38:48 +0530

    add urlencoded middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const PORT = 7000;
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.listen(PORT, () => {
   console.log(`Server is running`);
 });
