f8027e82826d6bbef782059cd8628541abf14932
Vishal Tiwari <vishaltiwari@makewebbetter.com>
2026-07-13 12:59:20 +0530

    Removed spaces

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser());
-
 app.use(cors());
-
 app.listen(PORT, () => {
   console.log(`Server is running`);
 });
