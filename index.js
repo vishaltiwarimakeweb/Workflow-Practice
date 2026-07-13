38c53aa64e4c456e3c197d209482157a748b5cf0
Vishal Tiwari <vishaltiwari@makewebbetter.com>
2026-07-13 12:52:32 +0530

    Added cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const PORT = 7000;
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.listen(PORT, () => {
   console.log(`Server is running`);
 });
