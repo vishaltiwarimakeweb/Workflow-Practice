a09837b32a088ebd7cd3143e0ec398483f2ace36
Vishal Tiwari <vishaltiwari@makewebbetter.com>
2026-07-13 12:54:04 +0530

    added cookie-parser

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const PORT = 7000;
+const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.listen(PORT, () => {
   console.log(`Server is running`);
 });
